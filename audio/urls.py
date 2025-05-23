b2b4ee53cf61102ff55b5737b2376f20f10c5199
Laxmi Narayan <148639069+laxmi-narayan-87@users.noreply.github.com>
2025-05-23 21:51:04 +0530

    Update urls.py

--- audio/urls.py
+++ audio/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('animation/', views.animation_view, name='animation'),
+    path('', views.home_view, name='home'), 
 ]
